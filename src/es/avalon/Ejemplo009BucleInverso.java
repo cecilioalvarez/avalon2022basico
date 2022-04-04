f31ff2b6443b0b4da1bb6ab2f5bb4e8d54afdd29
cecilioAlvarez <cmac@arquitecturajava.com>
2022-04-04 18:41:52 +0200

    bucles wile

---
+++ src/es/avalon/Ejemplo009BucleInverso.java
@@ -0,0 +1,13 @@
+package es.avalon;
+
+public class Ejemplo009BucleInverso {
+    public static void main(String[] args) {
+
+        int i=10;
+        while (i>0) {
+            System.out.println(i);
+            i--;
+
+        }
+    }
+}
