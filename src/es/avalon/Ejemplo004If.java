1bdde919d49d200b2d81f50d9efa666d8855a7c4
cecilioAlvarez <cmac@arquitecturajava.com>
2022-04-04 17:59:10 +0200

    sentencia if else

--- src/es/avalon/Ejemplo004If.java
+++ src/es/avalon/Ejemplo004If.java
@@ -1,4 +1,15 @@
 package es.avalon;
 
 public class Ejemplo004If {
+    public static void main(String[] args) {
+        int nota=5;
+
+        if (nota>=5) {
+            System.out.println("has aprobado");
+        }else {
+            System.out.println("has suspendido");
+        }
+
+
+    }
 }
