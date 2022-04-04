6e4e7011d789cd3b4a8f459aef1c69315315d5b4
cecilioAlvarez <cmac@arquitecturajava.com>
2022-04-04 16:40:15 +0200

    variable integer

---
+++ src/es/avalon/Ejemplo002Tipos.java
@@ -0,0 +1,12 @@
+package es.avalon;
+
+public class Ejemplo002Tipos {
+    public static void main (String args[]) {
+
+        int numero=7;
+        System.out.println(numero);
+        numero=100;
+        System.out.println(numero);
+
+    }
+}
