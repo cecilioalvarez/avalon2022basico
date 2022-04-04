bd0527a80c5b97b8a0cf381e3389d74d524d4aea
cecilioAlvarez <cmac@arquitecturajava.com>
2022-04-04 16:44:29 +0200

    todas las variables enteras

--- src/es/avalon/Ejemplo002Tipos.java
+++ src/es/avalon/Ejemplo002Tipos.java
@@ -7,6 +7,17 @@ public class Ejemplo002Tipos {
         System.out.println(numero);
         numero=100;
         System.out.println(numero);
-
+        byte numero1=7;
+        short numero2=2000;
+        int numero3=500000;
+        long numero4=50000000;
+        float numero5= 5000.23f;
+        double numero6= 5000000.3333;
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(numero4);
+        System.out.println(numero5);
+        System.out.println(numero6);
     }
 }
