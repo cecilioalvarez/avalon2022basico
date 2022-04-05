0c828ef1a52e7a11dcbb8597aa0d70cbd165b3bb
Wilson Valderrama <wilson.valderrama10@gmail.com>
2022-04-05 09:59:06 -0500

    ejercicio de Arrays

---
+++ src/es/avalon/EjemploArrays001.java
@@ -0,0 +1,11 @@
+package es.avalon;
+
+public class EjemploArrays001 {
+    public static void main(String[] args){
+        int[] lista = {10,20,30};
+
+        System.out.println(lista[0]);
+        System.out.println(lista[1]);
+        System.out.println(lista[2]);
+    }
+}
