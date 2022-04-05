e2ce9f607225a2d5f236c9bc6753fe4d42b03484
dcamposmagaldi <dcamposmagaldi@gmail.com>
2022-04-05 16:54:57 +0200

    Ejemplo Arrays

--- src/es/avalon/Ejemplo011Arrays.java
+++ src/es/avalon/Ejemplo011Arrays.java
@@ -5,5 +5,18 @@ import java.sql.SQLOutput;
 public class Ejemplo011Arrays {
     public static void main(String[] args) {
         System.out.println("Buenas tardes!!");
+        int [] lista= new int[6];
+        lista[0]= 0;
+        lista[1]= 10;
+        lista[2]= 20;
+        lista[3]= 30;
+        lista[4]= 40;
+        lista[5]= 50;
+        System.out.println(lista[0]);
+        System.out.println(lista[1]);
+        System.out.println(lista[2]);
+        System.out.println(lista[3]);
+        System.out.println(lista[4]);
+        System.out.println(lista[5]);
     }
 }
