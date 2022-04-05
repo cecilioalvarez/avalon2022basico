cfc4c2a49952148147109a74c655a46f1165300b
jcalat <jcalataruiz@hotmail.com>
2022-04-05 17:11:04 +0200

    Suma de Arrays

--- src/es/avalon/Ejemplo011Arrays.java
+++ src/es/avalon/Ejemplo011Arrays.java
@@ -2,15 +2,16 @@ package es.avalon;
 
 public class Ejemplo011Arrays {
     public static void main(String[] args) {
-        int [] lista=new int[3];
-        lista[0]=20;
-        lista[1]=30;
-        lista[2]=50;
+        int[] lista = new int[3];
+        lista[0] = 20;
+        lista[1] = 30;
+        lista[2] = 50;
         System.out.println(lista[0]);
         System.out.println(lista[1]);
         System.out.println(lista[2]);
-        for (int i=0;i< lista.length;i++){
+        for (int i = 0; i < lista.length; i++) {
             System.out.println(lista[i]);
         }
+
     }
 }
