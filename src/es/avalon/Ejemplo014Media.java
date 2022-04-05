342969939a07263eb7b48abea1815c1458e8a16f
jcalat <jcalataruiz@hotmail.com>
2022-04-05 18:13:16 +0200

    ejemplo14

---
+++ src/es/avalon/Ejemplo014Media.java
@@ -0,0 +1,16 @@
+package es.avalon;
+
+import javax.sound.midi.Soundbank;
+
+public class Ejemplo014Media {
+    public static void main(String[] args) {
+        double[] lista = new double[]{7, 10, 2, 5, 3, 0};
+        double suma = 0;
+        for (int i = 0; i < lista.length; i++) {
+            suma = suma + lista[i];
+
+        }
+        System.out.println("La nota media es: " + suma / lista.length);
+
+    }
+}
