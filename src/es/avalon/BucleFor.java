aec8f2fdf680e4aa52f8003cde9ce55ac73c2351
cecilioAlvarez <cmac@arquitecturajava.com>
2022-04-05 14:10:11 +0200

    bucle do while

---
+++ src/es/avalon/BucleFor.java
@@ -0,0 +1,15 @@
+package es.avalon;
+
+public class BucleFor {
+    public static void main(String[] args) {
+
+        int suma=0;
+
+        for (int i=0;i<10;i++) {
+            //acumulo valores en una variable suma
+            suma=suma+i;
+            System.out.println(i);
+        }
+        System.out.println(suma);
+    }
+}
