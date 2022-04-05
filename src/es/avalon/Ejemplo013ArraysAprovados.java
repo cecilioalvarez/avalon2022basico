f87adcb08788ddd01cdc398c414cd2d865959ba4
jcalat <jcalataruiz@hotmail.com>
2022-04-05 17:21:06 +0200

    ejemplo013

--- src/es/avalon/Ejemplo013ArraysAprovados.java
+++ src/es/avalon/Ejemplo013ArraysAprovados.java
@@ -2,14 +2,17 @@ package es.avalon;
 
 public class Ejemplo013ArraysAprovados {
     public static void main(String[] args) {
-        int [] lista= new int[]{7,10,2,1,3,2};
-        int aprobados=0;
-        for (int i=0;i< lista.length;i++){
-            if(lista[i]>=5){
+        int[] lista = new int[]{7, 10, 2, 1, 3, 2};
+        int aprobados = 0;
+
+        for (int i = 0; i < lista.length; i++) {
+            //Bucle for para recorrer la lista
+            if (lista[i] >= 5) {
                 aprobados++;
+            //Comparamos si el numero es mayor o igual a 5 y si es asi lo incluimos al acumuladir
             }
         }
-        System.out.println("Hay "+aprobados+" aprobados");
+        System.out.println("Hay " + aprobados + " aprobados");
 
     }
 }
