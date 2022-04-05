f614959082e4b1349f93813344058b1c27fa9155
cecilioAlvarez <cmac@arquitecturajava.com>
2022-04-05 18:32:23 +0200

    suma de cuadrados

--- src/es/avalon/EjemploMedia.java
+++ src/es/avalon/EjemploMedia.java
@@ -4,10 +4,21 @@ public class EjemploMedia {
     public static void main(String[] args) {
         int[] lista = new int[]{7,10,5,1,3,2};
         double suma=0;
+        double sumaCuadrados=0;
         for (int i=0;i<lista.length;i++) {
 
                 suma=suma+lista[i];
         }
+
+        for (int i=0;i<lista.length;i++) {
+
+            sumaCuadrados=sumaCuadrados+(lista[i]*lista[i]);
+        }
+
         System.out.println(suma/lista.length);
+        System.out.println(sumaCuadrados);
+
+
+
     }
 }
