875e5732137f3583777d17c3be48127866cc50ad
Juan Luis Villalon <jlvillalon3@gmail.com>
2022-04-05 17:11:26 +0200

    SumaArrays

--- src/es/avalon/Ejemplo011Arrays.java
+++ src/es/avalon/Ejemplo011Arrays.java
@@ -3,8 +3,22 @@ package es.avalon;
 public class Ejemplo011Arrays {
 
     public static void main(String[] args) {
-        //Prueba de rama
 
-        System.out.println("Prueba de funcionamiento");
+        //Declaracion de Array o lista
+        int[] lista = new int[3];
+
+        lista[0] = 20;
+        lista[1] = 30;
+        lista[2] = 50;
+
+        System.out.println(lista[0]);
+        System.out.println(lista[1]);
+        System.out.println(lista[2]);
+
+
+        for (int i=0; i<lista.length; i++) {
+            System.out.println(lista[i]);
+        }
+
     }
 }
