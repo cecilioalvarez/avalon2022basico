7d3dfdb513fbd8ff6be610e0f23e88839a6f673a
SirEikir <cristoferpuerto@gmail.com>
2022-04-05 16:53:43 +0200

    Ejemplo Array Erik

--- src/es/avalon/Ejemplo011Arrays.java
+++ src/es/avalon/Ejemplo011Arrays.java
@@ -1,8 +1,13 @@
 package es.avalon;
 
 public class Ejemplo011Arrays {
-
     public static void main(String[] args) {
-        //Hola Soy Erik
+        int [] lista= new int [3];
+        lista[0]= 20;
+        lista[1]= 30;
+        lista[2]= 50;
+        System.out.println(lista[0]);
+        System.out.println(lista[1]);
+        System.out.println(lista[2]);
     }
 }
