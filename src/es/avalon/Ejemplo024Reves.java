501e2b199af380b110de2f5122fa8dac29116787
cecilioAlvarez <cmac@arquitecturajava.com>
2022-04-06 17:16:01 +0200

    ejemplo del reves

---
+++ src/es/avalon/Ejemplo024Reves.java
@@ -0,0 +1,14 @@
+package es.avalon;
+
+public class Ejemplo024Reves {
+    public static void main(String[] args) {
+
+        int [] lista = new int []{2,2,6,8,1,2,3,6};
+
+        for (int i= lista.length-1;i>=0;i--) {
+
+            System.out.println(lista[i]);
+        }
+
+    }
+}
