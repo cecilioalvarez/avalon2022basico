97e61a039ec7d8f45dd65c70267362d5f1cfcaa9
jcalat <jcalataruiz@hotmail.com>
2022-04-06 18:41:24 +0200

    mayor cadena

--- src/es/avalon/Ejemplo025ArraysAnidados.java
+++ src/es/avalon/Ejemplo025ArraysAnidados.java
@@ -2,11 +2,11 @@ package es.avalon;
 
 public class Ejemplo025ArraysAnidados {
     public static void main(String[] args) {
-        int[]lista1=new int[]{1,2,3};
-        int[]lista2=new int[]{4,5,6};
+        int[] lista1 = new int[]{1, 2, 3};
+        int[] lista2 = new int[]{4, 5, 6};
 
-        int[][]listaArrays=new int[][]{lista1,lista2};  //fila 0--> 1,2,3
-                                                        //fila 1--> 4,5,6
+        int[][] listaArrays = new int[][]{lista1, lista2};  //fila 0--> 1,2,3
+        //fila 1--> 4,5,6
 //        System.out.println("Coordenada 0,0 = "+listaArrays[0][0]);
 //        System.out.println("Coordenada 0,1 = "+listaArrays[0][1]);
 //        System.out.println("Coordenada 0,2 = "+listaArrays[0][2]);
@@ -15,24 +15,24 @@ public class Ejemplo025ArraysAnidados {
 //        System.out.println("Coordenada 1,2 = "+listaArrays[1][2]);
 
         System.out.println("...............");
-        for (int i=0;i< listaArrays.length;i++){
-            for (int j=0;j< listaArrays[i].length;j++){
+        for (int i = 0; i < listaArrays.length; i++) {
+            for (int j = 0; j < listaArrays[i].length; j++) {
                 System.out.println(listaArrays[i][j]);
             }
         }
 
         System.out.println("...............");
-        int suma=0;
-        for (int i=0;i< listaArrays.length;i++){
-            for (int j=0;j< listaArrays[i].length;j++){
-                suma=suma+listaArrays[i][j];
+        int suma = 0;
+        for (int i = 0; i < listaArrays.length; i++) {
+            for (int j = 0; j < listaArrays[i].length; j++) {
+                suma = suma + listaArrays[i][j];
 
             }
         }
         System.out.println(suma);
-        for (int i=0;i< listaArrays.length;i++){
-            for (int j=0;j< listaArrays[i].length;j++){
-                System.out.println("la coordenada ["+i+"],["+j+"]=["+listaArrays[i][j]+"]");
+        for (int i = 0; i < listaArrays.length; i++) {
+            for (int j = 0; j < listaArrays[i].length; j++) {
+                System.out.println("la coordenada [" + i + "],[" + j + "]=[" + listaArrays[i][j] + "]");
 
             }
         }
---
+++ src/es/avalon/Ejemplo026ArraysCadenas.java
@@ -0,0 +1,19 @@
+package es.avalon;
+
+public class Ejemplo026ArraysCadenas {
+    public static void main(String[] args) {
+        String [] lista=new String[]{"Hola","que","tal","estas"};
+        String cadena= lista[0];
+        String cadena1= lista[0];
+        for (int i=0;i< lista.length;i++) {
+            if (cadena.length() < lista[i].length()) {
+                cadena = lista[i];
+
+            }
+        }
+        System.out.println(cadena);
+
+
+
+    }
+}
