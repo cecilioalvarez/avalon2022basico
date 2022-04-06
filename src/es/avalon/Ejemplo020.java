33e82b5fc2ab1937440e9a0db6142b82235e9f0b
Edwin <edwinstiv20@gmail.com>
2022-04-06 08:57:21 -0500

    suma funcion

--- src/es/avalon/Ejemplo020.java
+++ src/es/avalon/Ejemplo020.java
@@ -5,7 +5,18 @@ public class Ejemplo020 {
 
         int [] numeros= new int[]{2,5,7,9,8,1,2};
         int [] numeros2= new int[]{2,5,7,9,8,1,2};
-
+        int menor= numeros[0];
+        for (int i=0;i< numeros.length;i++){
+            if (menor>numeros[i]){
+                menor=numeros[i];
+            }
+        }
+        for (int i=0;i< numeros.length;i++){
+            if (menor>numeros2[i]){
+                menor=numeros2[i];
+            }
+        }
+        System.out.println(menor);
 
 
     }
