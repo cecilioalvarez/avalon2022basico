e39fcc21d8c4f384e2924926cbc7ac137f9a1a90
cecilioAlvarez <cmac@arquitecturajava.com>
2022-04-06 16:19:27 +0200

    añadir codigo

--- src/es/avalon/Ejemplo21NumeroMenor.java
+++ src/es/avalon/Ejemplo21NumeroMenor.java
@@ -1,26 +1,28 @@
 package es.avalon;
 
-public class Ejemplo020NumeroMayor {
+public class Ejemplo21NumeroMenor {
+
     public static void main(String[] args) {
 
         int[] numeros= new int[] {2,5,7,9,8,1,2};
         int[] numeros2= new int[] {7,5,7,9,10,1,2};
 
-        int mayor=0;
+        int menor=numeros[0];
 
         for (int i=0;i<numeros.length;i++) {
 
-            if (mayor<numeros[i]) {
-                mayor=numeros[i];
+            if (menor>numeros[i]) {
+                menor=numeros[i];
             }
         }
 
         for (int i=0;i<numeros2.length;i++) {
 
-            if (mayor<numeros2[i]) {
-                mayor=numeros2[i];
+            if (menor>numeros2[i]) {
+                menor=numeros2[i];
             }
         }
-        System.out.println(mayor);
+        System.out.println(menor);
+
     }
 }
