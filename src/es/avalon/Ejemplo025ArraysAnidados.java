d66e6a2d3c25bcecae89f6d8187f1029908fb7a0
Juan Luis Villalon <jlvillalon3@gmail.com>
2022-04-06 18:14:27 +0200

    Arrays Anidados Ejercicios

--- src/es/avalon/Ejemplo025ArraysAnidados.java
+++ src/es/avalon/Ejemplo025ArraysAnidados.java
@@ -13,12 +13,32 @@ public class Ejemplo025ArraysAnidados {
         //Acceso a este
         System.out.println(listaArrays[0][0]); //Accedemos al primer array guardado [0] y la primera posicion de este [0]
 
-        System.out.println("*************");
+        System.out.println("Acceso a array anidado");
 
         for(int i=0; i<listaArrays.length; i++){
             for (int j=0; j<listaArrays[i].length; j++){
                 System.out.println(listaArrays[i][j]);
             }
         }
+
+        //Sumar todos los items del array
+        int sumaNumero = 0;
+        for(int i=0; i<listaArrays.length; i++){
+            for (int j=0; j<listaArrays[i].length; j++){
+                sumaNumero+= listaArrays[i][j];
+            }
+        }
+        System.out.println("Sumar todos los items del array");
+        System.out.println(sumaNumero);
+
+        System.out.println("Sacar por consola posicion item y dato de cada posicion");
+
+        //Sacar por consola posicion item y dato de cada posicion
+        for(int i=0; i<listaArrays.length; i++){
+            for (int j=0; j<listaArrays[i].length; j++){
+                System.out.println("[" + i + "," + j + "]" + "=" + listaArrays[i][j]);
+            }
+        }
+
     }
 }
