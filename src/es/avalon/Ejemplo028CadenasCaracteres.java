09fdf5e610a827287ba69a0826fbfdc9d040979b
jcalat <jcalataruiz@hotmail.com>
2022-04-06 19:01:37 +0200

    manejo de arrays de cadenas

---
+++ src/es/avalon/Ejemplo028CadenasCaracteres.java
@@ -0,0 +1,13 @@
+package es.avalon;
+
+public class Ejemplo028CadenasCaracteres {
+    public static void main(String[] args) {
+        String[] lista = new String[]{"Hola", "que", "tal", "estas", "tu"};
+        for (int i = 0; i < lista.length; i++) {
+            for (int j = 0; j < lista[i].length(); j++) {
+                char caracter = lista[i].charAt(j);
+                System.out.println(caracter);
+            }
+        }
+    }
+}
