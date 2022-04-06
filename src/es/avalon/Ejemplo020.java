e0790760dbb6fa83035c525fa53d5385eda0d943
jcalat <jcalataruiz@hotmail.com>
2022-04-06 16:43:40 +0200

    suma de elementos en nuevo array

--- src/es/avalon/Ejemplo020.java
+++ src/es/avalon/Ejemplo020.java
@@ -2,18 +2,14 @@ package es.avalon;
 
 public class Ejemplo020 {
     public static void main(String[] args) {
-        int[] numeros = new int[]{78, 57, 7, 98, 100, 8, 112};
-        int[] numeros2 = new int[]{22, 5, 74, 9, 10, 88, 277};
-        if (maspequeño(numeros) <= maspequeño(numeros2)) {
-            System.out.println(maspequeño(numeros));
-        } else {
-            System.out.println(maspequeño(numeros2));
-        }
+        int[] numeros = new int[]{78, 57, 8, 98, 100, 8, 112};
+        int[] numeros2 = new int[]{22, 4, 74, 9, 10, 88, 277};
 
+        System.out.println(maspequeño(numeros, numeros2));
 
     }
 
-    static int maspequeño(int[] a) {
+    static int maspequeño(int[] a, int[] b) {
         int num1 = a[1];
         for (int i = 0; i < a.length; i++) {
 
@@ -21,6 +17,19 @@ public class Ejemplo020 {
                 num1 = a[i];
             }
         }
+        int num2 = b[1];
+        for (int i = 0; i < b.length; i++) {
+
+            if (num2 > a[i]) {
+                num2 = a[i];
+            }
+        }
+        if (num1 >= num2) {
+            num1 = num2;
+        }
         return num1;
     }
+
+
+
 }
---
+++ src/es/avalon/Ejemplo021SumaElementosArray.java
@@ -0,0 +1,19 @@
+package es.avalon;
+
+public class Ejemplo021SumaElementosArray {
+    public static void main(String[] args) {
+        int[] numeros = new int[]{78, 57, 8, 98, 100, 8, 112};
+        int[] numeros2 = new int[]{22, 4, 74, 9, 10, 88, 277};
+        int[] sumaElementos=new int [7];
+        sumadearrays(numeros, numeros2, sumaElementos);
+    }
+
+    private static void sumadearrays(int[] numeros, int[] numeros2, int[] sumaElementos) {
+        for (int i=0;i<7;i++){
+            sumaElementos[i]= numeros[i]+ numeros2[i];
+        }
+        for (int i = 0; i< sumaElementos.length; i++){
+            System.out.println(sumaElementos[i]);
+        }
+    }
+}
