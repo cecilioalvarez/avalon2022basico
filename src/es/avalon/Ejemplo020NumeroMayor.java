f565eacc09f7be184c94f5d6ae92fb828db1237f
cecilioAlvarez <cmac@arquitecturajava.com>
2022-04-06 15:48:48 +0200

    renombrar mayor arrays

--- src/es/avalon/Ejemplo020NumeroMayor.java
+++ src/es/avalon/Ejemplo020NumeroMayor.java
@@ -1,6 +1,6 @@
 package es.avalon;
 
-public class Ejemplo020 {
+public class Ejemplo020NumeroMayor {
     public static void main(String[] args) {
 
         int[] numeros= new int[] {2,5,7,9,8,1,2};
