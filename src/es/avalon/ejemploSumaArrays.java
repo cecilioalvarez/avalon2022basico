43fa234f6ee5b2310de27bfa7177732f3d35173b
dcamposmagaldi <dcamposmagaldi@gmail.com>
2022-04-06 16:58:30 +0200

    ejemplo de funciones

---
+++ src/es/avalon/ejemploSumaArrays.java
@@ -0,0 +1,21 @@
+package es.avalon;
+
+public class ejemploSumaArrays {
+    public static void main(String[] args) {
+
+        int[] numero1 = new int[]{1, 2, 4, 5, 6, 4, 6, 8, 6, 8, 9, 6, 43,4};
+        int[] numero2 = new int[]{7, 8, 6, 7, 0, 5, 4, 3, 57, 8, 4, 3, 3, 67};
+        int[] sumaElementos = new int[14];
+
+        for (int i=0; i<numero1.length; i++ ){
+            sumaElementos[i]=numero1[i]+numero2[i];
+        }
+
+        for (int i=0; i<sumaElementos.length;i++){
+            System.out.println(sumaElementos[i]);
+
+        }
+    }
+
+
+}
