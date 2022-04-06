985cbba39b57be1a98196422f7334b20d3fae915
alejandro <yesid.alejo300@gmail.com>
2022-04-06 07:32:27 -0500

    funciones

---
+++ src/Ejemplo017Funciones.java
@@ -0,0 +1,12 @@
+public class Ejemplo017Funciones {
+    public static void main(String[] args) {
+
+        System.out.println(sumar(2,3));
+        System.out.println(sumar(7,8));
+        System.out.println(sumar(4,10));
+    }
+    static double  sumar(double a, double b){
+
+        return a+b;
+    }
+}
