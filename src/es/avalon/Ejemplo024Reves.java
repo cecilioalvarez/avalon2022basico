a173b8d6b50db35ce7557ca04b02e77b04e8a2ec
jcalat <jcalataruiz@hotmail.com>
2022-04-06 17:14:42 +0200

    array al reves

---
+++ src/es/avalon/Ejemplo024Reves.java
@@ -0,0 +1,15 @@
+package es.avalon;
+
+public class Ejemplo024Reves {
+    public static void main(String[] args) {
+        int[] numeros = new int[]{78, 57, 8, 98, 100, 8, 112};
+        arrayinverso(numeros);
+
+    }
+
+    private static void arrayinverso(int[] numeros) {
+        for (int i = numeros.length-1; i>=0; i--){
+            System.out.println(numeros[i]);
+        }
+    }
+}
