6e1ebb050d55ef2bcc19413eafa8ba07f5f81bdf
alejandro <yesid.alejo300@gmail.com>
2022-04-06 08:00:29 -0500

    funciones

---
+++ src/Ejemplo019.java
@@ -0,0 +1,26 @@
+public class Ejemplo019 {
+    public static void main(String[] args) {
+        int[] numer = new int[]{2,5,7,9,10,1,2};
+        System.out.println("aprobados: "+ aprobados(numer));
+        System.out.println("No aprobados: "+ noAprobados(numer));
+    }
+    static double aprobados(int [] lista){
+        double total=0;
+        for (int i = 0; i < lista.length; i++) {
+            if (lista[i]>=5){
+                total++;
+            }
+        }
+        return total;
+    }
+
+    static double noAprobados(int [] lista){
+        double total=0;
+        for (int i = 0; i < lista.length; i++) {
+            if (lista[i]<5){
+                total++;
+            }
+        }
+        return total;
+    }
+}
