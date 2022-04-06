782652edb0a4248d44006cb92e8cbfa10e4be61c
alejandro <yesid.alejo300@gmail.com>
2022-04-06 10:26:36 -0500

    funciones

---
+++ src/Ejemplo0020Repaso.java
@@ -0,0 +1,22 @@
+public class Ejemplo0020Repaso {
+    public static void main(String[] args) {
+        int[] numer = new int[]{2,5,7,9,8,1,2};
+        int[] numer2 = new int[]{2,5,7,9,10,1,2};
+        System.out.println(menor(numer,numer2));
+    }
+    static int menor(int[] uno, int [] dos){
+        int menor=999999999;
+        for (int i = 0; i < uno.length; i++) {
+            if(menor>uno[i]){
+                menor=uno[i];
+            }
+        }
+        for (int i = 0; i < dos.length; i++) {
+            if(menor>dos[i]){
+                menor=dos[i];
+            }
+        }
+        return menor;
+    }
+
+}
