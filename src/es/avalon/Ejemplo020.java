b4a5cdcdb35abe9da10d7156242896dfcdc0805e
cecilioAlvarez <cmac@arquitecturajava.com>
2022-04-06 15:47:03 +0200

    inicio 20

---
+++ src/es/avalon/Ejemplo020.java
@@ -0,0 +1,11 @@
+package es.avalon;
+
+public class Ejemplo020 {
+    public static void main(String[] args) {
+
+        int[] numeros= new int[] {2,5,7,9,8,1,2};
+        int[] numeros2= new int[] {7,5,7,9,10,1,2};
+
+
+    }
+}
