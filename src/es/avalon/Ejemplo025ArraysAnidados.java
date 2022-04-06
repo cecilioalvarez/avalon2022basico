212153f641dbe3883a3ad2a62204120f5dd326fe
SirEikir <cristoferpuerto@gmail.com>
2022-04-06 18:01:40 +0200

    Array de Arrays

--- src/es/avalon/Ejemplo025ArraysAnidados.java
+++ src/es/avalon/Ejemplo025ArraysAnidados.java
@@ -6,12 +6,16 @@ public class Ejemplo025ArraysAnidados {
         int[] lista2 = new int[]{4,5,6};
 
         int[][] listaArrays= new int[][] {lista1,lista2};
+        int suma=0;
 
         for (int i =0;i<listaArrays.length;i++){
             for(int j = 0;j< listaArrays[i].length;j++){
+                suma= suma + listaArrays[i][j];
                 System.out.print(listaArrays[i][j]);
             }
         }
+        System.out.println();
+        System.out.println(suma);
 
     }
 }
