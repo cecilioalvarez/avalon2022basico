5afe7aa34e7b3fcaf5d79cf02e9bc8a358994e92
Edwin <edwinstiv20@gmail.com>
2022-04-06 08:51:42 -0500

    suma funcion

---
+++ src/es/avalon/Ejemplo020.java
@@ -0,0 +1,12 @@
+package es.avalon;
+
+public class Ejemplo020 {
+    public static void main(String[] args){
+
+        int [] numeros= new int[]{2,5,7,9,8,1,2};
+        int [] numeros2= new int[]{2,5,7,9,8,1,2};
+
+
+
+    }
+}
