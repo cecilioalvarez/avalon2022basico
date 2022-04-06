007aeec021365b63ac44f6d44a3c2e4cf393412e
Edwin <edwinstiv20@gmail.com>
2022-04-06 09:16:08 -0500

    suma funcion

---
+++ src/es/avalon/Ejemplo016ArrayCadenas.java
@@ -0,0 +1,13 @@
+package es.avalon;
+
+public class Ejemplo016ArrayCadenas {
+    public static void main(String[] args) {
+
+        String [] listaTextos= new String[] { "hola","que","tal","estas"};
+
+        for (int i=0;i<listaTextos.length;i++) {
+            System.out.println(listaTextos[i]);
+        }
+
+    }
+}
