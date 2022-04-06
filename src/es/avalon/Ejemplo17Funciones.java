a38a5fb032843cb88871fe597f72da7424ab493f
Edwin <edwinstiv20@gmail.com>
2022-04-06 09:17:38 -0500

    suma funcion

---
+++ src/es/avalon/Ejemplo17Funciones.java
@@ -0,0 +1,32 @@
+package es.avalon;
+
+public class Ejemplo17Funciones {
+    public static void main(String[] args) {
+
+
+        System.out.println(sumar(2,3));
+        System.out.println(restar(4,6));
+        System.out.println(multiplicar(7,8));
+        System.out.println(dividir(7,8));
+
+    }
+    //la reutilizacion
+    static double  sumar(double a , double b) {
+
+        return a+b;
+    }
+
+    static double  restar(double a , double b) {
+
+        return a-b;
+    }
+    static double  multiplicar(double a , double b) {
+
+        return a*b;
+    }
+    static double  dividir(double a , double b) {
+
+        return a/b;
+    }
+
+}
