8e43de1558f1e56edc1b11965b7089f6350b593f
Edwin <edwinstiv20@gmail.com>
2022-04-06 11:50:36 -0500

    suma funcion

---
+++ src/es/avalon/Ejemplo028CadenasCaracteres.java
@@ -0,0 +1,15 @@
+package es.avalon;
+
+public class Ejemplo028CadenasCaracteres {
+    public static void main(String[] args){
+
+        String[] lista= new String[]{"hola","que","tal","estas","tu"};
+        String mayor="";for(int i=0;i<lista.length;i++){
+
+            for (int j=0;j<lista[i].length();j++){
+                System.out.println(lista[i].charAt(j));
+            }
+        }
+
+    }
+}
