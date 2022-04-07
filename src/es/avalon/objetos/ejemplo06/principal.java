b0941cc4a428add30370710783c2d28a991eefb6
Edwin <edwinstiv20@gmail.com>
2022-04-07 12:00:22 -0500

    rectangulos

--- src/es/avalon/objetos/ejemplo06/principal.java
+++ src/es/avalon/objetos/ejemplo06/principal.java
@@ -3,5 +3,11 @@ package es.avalon.objetos.ejemplo06;
 public class principal {
     public static void main(String[] args) {
 
+        Rectangulo r1= new Rectangulo(2,3);
+        System.out.println(r1.area());
+        System.out.println(r1.perimetro());
 
-}}
+        r1.setLado1(200);
+        System.out.println(r1.perimetro());
+
+    }}
