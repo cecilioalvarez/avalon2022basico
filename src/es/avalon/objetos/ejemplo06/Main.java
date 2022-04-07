4c8c2e169db27f171bfd3bfeee0ac78ecfe38909
SirEikir <cristoferpuerto@gmail.com>
2022-04-07 18:20:01 +0200

    Ejercicio Rectangulo Resuelto

---
+++ src/es/avalon/objetos/ejemplo06/Main.java
@@ -0,0 +1,9 @@
+package es.avalon.objetos.ejemplo06;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangulo rectangulo = new Rectangulo(2,3);
+        System.out.println(rectangulo.area());
+        System.out.println(rectangulo.perimetro());
+    }
+}
