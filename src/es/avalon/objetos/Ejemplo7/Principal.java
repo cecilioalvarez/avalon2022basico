3635d42e94fd4bfaa08c74a85f7f12fe0c4e9051
jcalat <jcalataruiz@hotmail.com>
2022-04-07 18:27:43 +0200

    Lampara de techo

--- src/es/avalon/objetos/Ejemplo7/Principal.java
+++ src/es/avalon/objetos/Ejemplo7/Principal.java
@@ -3,6 +3,6 @@ package es.avalon.objetos.Ejemplo7;
 public class Principal {
     public static void main(String[] args) {
         LamparaTecho L1=new LamparaTecho(60,50,23);
-        System.out.println(L1.potenciaTotal());
+        System.out.println(L1.potenciaTotal());//kk
     }
 }
