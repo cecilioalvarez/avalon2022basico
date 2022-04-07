4fdf33a5791b2a23a116ce0e00d2da0413981fb8
jcalat <jcalataruiz@hotmail.com>
2022-04-07 18:14:56 +0200

    Lampara de techo

--- src/es/avalon/basico/Ejemplo013ArraysAprobados.java
+++ src/es/avalon/basico/Ejemplo013ArraysAprobados.java
@@ -9,10 +9,29 @@ public class Ejemplo013ArraysAprobados {
             //Bucle for para recorrer la lista
             if (lista[i] >= 5) {
                 aprobados++;
-            //Comparamos si el numero es mayor o igual a 5 y si es asi lo incluimos al acumuladir
+                //Comparamos si el numero es mayor o igual a 5 y si es asi lo incluimos al acumuladir
             }
         }
         System.out.println("Hay " + aprobados + " aprobados");
 
+        boolean test = false;
+        do {
+            int temp;
+            test = true;
+            for (int i = 0; i < lista.length-1; i++) {
+                if (lista[i] > lista[i + 1]) {
+                    temp = lista[i];
+                    lista[i] = lista[i + 1];
+                    test = false;
+
+                }
+            }
+        }while (test);
+        for (int i=0;i< lista.length;i++){
+            System.out.println("------"+lista[i]);
+        }
     }
 }
+
+
+
