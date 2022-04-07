300518258de207b455c82744848adbfe73027fdd
jcalat <jcalataruiz@hotmail.com>
2022-04-07 14:38:22 +0200

    ejemploIVA

--- src/es/avalon/basico/Ejemplo016ArrayCadenas.java
+++ src/es/avalon/basico/Ejemplo016ArrayCadenas.java
@@ -1,6 +1,6 @@
 package es.avalon.basico;
 
-public class ejemplo016ArrayCadenas {
+public class Ejemplo016ArrayCadenas {
     public static void main(String[] args) {
         String [] listaTextos=new String[] {"Hola ","que ","tal ","estas"};
         for (int i=0;i< listaTextos.length;i++){
