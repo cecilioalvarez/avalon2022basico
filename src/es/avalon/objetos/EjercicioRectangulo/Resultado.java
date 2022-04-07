f5c5299c91a50b44837ecca7b13929bd10caf471
dcamposmagaldi <dcamposmagaldi@gmail.com>
2022-04-07 17:25:17 +0200

    Ejericio Rectangulo

--- src/es/avalon/objetos/EjercicioRectangulo/Resultado.java
+++ src/es/avalon/objetos/EjercicioRectangulo/Resultado.java
@@ -2,7 +2,7 @@ package es.avalon.objetos.EjercicioRectangulo;
 
 public class Resultado {
     public static void main(String[] args) {
-        Rectangulo rec= new Rectangulo(25,10);
+        Rectangulo rec= new Rectangulo(25,2.5);
 
         System.out.println(rec.area());
         System.out.println(rec.perimetro());
