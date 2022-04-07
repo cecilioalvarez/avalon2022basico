d50b68b9e0c7b34b7316f297e25b3b5226213e21
cecilioAlvarez <cmac@arquitecturajava.com>
2022-04-07 18:56:21 +0200

    bombillas arrays lampara

---
+++ src/es/avalon/objetos/ejemplo06/Principal.java
@@ -0,0 +1,14 @@
+package es.avalon.objetos.ejemplo06;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Rectangulo r1= new Rectangulo(2,3);
+        System.out.println(r1.area());
+        System.out.println(r1.perimetro());
+
+        r1.setLado1(200);
+        System.out.println(r1.perimetro());
+
+    }
+}
---
+++ src/es/avalon/objetos/ejemplo07/Principal.java
@@ -0,0 +1,9 @@
+package es.avalon.objetos.ejemplo07;
+
+public class Principal {
+    public static void main(String[] args) {
+        LamparaTecho lt= new LamparaTecho(100,50,50);
+        System.out.println(lt.potenciaTotalBombillas());
+
+    }
+}
