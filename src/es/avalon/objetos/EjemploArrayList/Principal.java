5b11b29b534c9ba6b97d5223a3fb3d7b476d283b
cecilioAlvarez <cmac@arquitecturajava.com>
2022-04-08 15:05:44 +0200

    faltaba parentesiss

--- src/es/avalon/objetos/EjemploArrayList/Principal.java
+++ src/es/avalon/objetos/EjemploArrayList/Principal.java
@@ -7,7 +7,7 @@ public class Principal {
     public static void main(String[] args) {
 
         ArrayList<Telefono> lista= new ArrayList<Telefono>();
-        lista.add(new Telefono(123456789, "apple");
+        lista.add(new Telefono(123456789, "apple"));
 
 
     }
