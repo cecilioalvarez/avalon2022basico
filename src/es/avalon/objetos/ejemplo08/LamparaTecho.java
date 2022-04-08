6d7a2ad84b66fda017d9bd9545f59757135f21e4
cecilioAlvarez <cmac@arquitecturajava.com>
2022-04-08 15:01:02 +0200

    pendientes

---
+++ src/es/avalon/objetos/ejemplo08/LamparaTecho.java
@@ -0,0 +1,33 @@
+package es.avalon.objetos.ejemplo08;
+
+public class LamparaTecho {
+
+    private int[] bombillas;
+
+    public int[] getBombillas() {
+        return bombillas;
+    }
+
+    public void setBombillas(int[] bombillas) {
+        this.bombillas = bombillas;
+    }
+    public void setBombilla(int posicion, int potencia) {
+        if (posicion>=0 && posicion< bombillas.length)
+        this.bombillas[posicion]=potencia;
+    }
+
+    public LamparaTecho(int numeroBombillas, int potenciaBombilla) {
+        bombillas = new int[numeroBombillas];
+        for (int i = 0; i < bombillas.length; i++) {
+            bombillas[i] = potenciaBombilla;
+        }
+
+    }
+    public int potenciaTotal() {
+        int total = 0;
+        for (int i = 0; i < bombillas.length; i++) {
+            total += bombillas[i];
+        }
+    return total;
+    }
+}
