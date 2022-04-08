5abfea07112a98a3c7f4ef7f7363dd9b42c97373
Wilson Valderrama <wilson.valderrama10@gmail.com>
2022-04-08 09:42:14 -0500

    Ejercicio 009

---
+++ src/es/avalon/objetos/EjemploObjetos009/Persona.java
@@ -0,0 +1,67 @@
+package es.avalon.objetos.EjemploObjetos009;
+
+import java.util.ArrayList;
+
+public class Persona {
+    private String dni;
+    private String nombre;
+    private int edad;
+    private Telefono telefono;
+
+    private ArrayList<Chaqueta> chaquetas= new ArrayList<Chaqueta>();
+
+    public ArrayList<Chaqueta> getChaquetas() {
+        return chaquetas;
+    }
+
+    public void addChaqueta(Chaqueta chaqueta) {
+
+        chaquetas.add(chaqueta);
+    }
+
+    public void setChaquetas(ArrayList<Chaqueta> chaquetas) {
+        this.chaquetas = chaquetas;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public Telefono getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Telefono telefono) {
+        this.telefono = telefono;
+    }
+
+    public Persona(String dni, String nombre, int edad) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public Persona(String dni) {
+        this.dni = dni;
+    }
+}
---
+++ src/es/avalon/objetos/EjemploObjetos009/Telefono.java
@@ -0,0 +1,28 @@
+package es.avalon.objetos.EjemploObjetos009;
+
+public class Telefono {
+    private String marca;
+    private int numero;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Telefono(String marca, int numero) {
+        setMarca(marca);
+        setNumero(numero);
+    }
+}
+
