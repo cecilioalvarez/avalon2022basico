0332ffd849b8e478bb458a7d729c4610e80f8e39
SirEikir <cristoferpuerto@gmail.com>
2022-04-08 14:42:22 +0200

    Uso de clases externas con constructores

--- src/es/avalon/objetos/ejemplo10/Main.java
+++ src/es/avalon/objetos/ejemplo10/Main.java
@@ -5,5 +5,8 @@ public class Main {
         Persona p1 = new Persona();
         Persona p2= new Persona(23, "Rosa");
         Persona p3= new Persona(25);
+        Telefono t1= new Telefono ("Xiaomi", 1234562341);
+        p2.setTelefono(t1);
+        System.out.println(p2.getTelefono().getNumero());
     }
 }
--- src/es/avalon/objetos/ejemplo10/Persona.java
+++ src/es/avalon/objetos/ejemplo10/Persona.java
@@ -3,7 +3,24 @@ package es.avalon.objetos.ejemplo10;
 public class Persona {
     private int edad;
     private String nombre;
+    private int dni;
+    private Telefono telefono;
 
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public Telefono getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(Telefono telefono) {
+        this.telefono = telefono;
+    }
 
     public int getEdad() {
         return edad;
@@ -31,6 +48,13 @@ public class Persona {
     public Persona(int edad, String nombre) {
         this.edad = edad;
         this.nombre = nombre;
+    }
+
+    public Persona(int edad, String nombre, int dni, Telefono telefono) {
+        this.edad = edad;
+        this.nombre = nombre;
+        this.dni = dni;
+        this.telefono = telefono;
     }
 }
 
