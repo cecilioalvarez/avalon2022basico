ea45ce8839a209b57cf6fbc0ade440d6826eea54
dcamposmagaldi <dcamposmagaldi@gmail.com>
2022-04-11 19:46:26 +0200

    Creada clase Nota (Corregido por David)

--- src/es/avalon/objetos/ejemplo03/Principal.java
+++ src/es/avalon/objetos/ejemplo03/Principal.java
@@ -3,11 +3,11 @@ package es.avalon.objetos.ejemplo03;
 public class Principal {
     public static void main(String[] args) {
 
-      /*  Nota nota1 = new Nota();
+        Nota nota1 = new Nota();
         nota1.setNota(700);
         nota1.setAsignatura("matematicas");
         System.out.println(nota1.getNota());
         System.out.println(nota1.getAsignatura());
-        System.out.println(nota1.estaAprobada());*/
+        System.out.println(nota1.estaAprobada());
     }
 }
