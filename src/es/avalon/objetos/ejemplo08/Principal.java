8c50e1f47a8eaccc9e0d96a71a457e0d45683d9f
Nerys <beatrizalvaradocona@gmail.com>
2022-04-11 17:35:51 -0400

    Bombilla Array Lampara

---
+++ src/es/avalon/objetos/ejemplo08/Principal.java
@@ -0,0 +1,14 @@
+package es.avalon.objetos.ejemplo08;
+
+public class Principal {
+    public static void main(String[] args){
+        lamparatecho lt= new lamparatecho(5,150);
+        System.out.println(lt.pTotal());
+
+        lt.setBomb(5,150);
+        System.out.println(lt.pTotal());
+
+
+
+    }
+}
