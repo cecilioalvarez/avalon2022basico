b9dcf0aeeb718e0d199bfcab4324f9da13709bd2
dcamposmagaldi <dcamposmagaldi@gmail.com>
2022-04-11 16:42:05 +0200

    Ejemplo Clase matemáticas

--- src/es/avalon/objetos/Ejemplo017/Main.java
+++ src/es/avalon/objetos/Ejemplo017/Main.java
@@ -3,6 +3,7 @@ package es.avalon.objetos.Ejemplo017;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Matematicas.sumar(2,2));
+        double resultado=Matematicas.sumar(2,2);
+        System.out.println(resultado);
     }
 }
