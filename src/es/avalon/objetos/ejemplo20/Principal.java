1c4159c02d3bac645ac339a35b70aca815209f23
Edwin <edwinstiv20@gmail.com>
2022-04-11 11:16:32 -0500

    sinivass

---
+++ src/es/avalon/objetos/ejemplo20/Principal.java
@@ -0,0 +1,10 @@
+package es.avalon.objetos.ejemplo20;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Persona p= new Persona("12345678a","pedro");
+        System.out.println(p);
+
+    }
+}
