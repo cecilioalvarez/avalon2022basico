efe4343911f70fd01166f0b7e1a732fb37195721
Mercedes <mercedes.alfonzo20@gmail.com>
2022-04-11 17:39:48 -0400

    Multiples Constructores

---
+++ src/es/avalon/objeto/ejemplo09/principal.java
@@ -0,0 +1,21 @@
+package es.avalon.objeto.ejemplo09;
+
+public class principal {
+
+
+    public static void main(String[] args){
+
+        // int num=7;
+        // num++;
+        //System.out.println(num);
+        //num++;
+        // System.out.println(num);
+
+        int numero=0;
+        for (int i=0;i<10;i++){
+            System.out.println(numero+i);
+        }
+    }
+
+
+}
