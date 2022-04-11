e360d9c4c6eac945ab05340fbe5299c6951a80c5
cecilioAlvarez <cmac@arquitecturajava.com>
2022-04-11 16:32:48 +0200

    clase de matematicas

---
+++ src/es/avalon/objetos/ejemplo17/Matematicas.java
@@ -0,0 +1,24 @@
+package es.avalon.objetos.ejemplo17;
+
+public class Matematicas {
+
+    public double sumar (int n1, int n2) {
+
+        return n1+n2;
+    }
+
+    public double restar (int n1, int n2) {
+
+        return n1-n2;
+    }
+
+    public double producto (int n1, int n2) {
+
+        return n1*n2;
+    }
+
+    public double division (int n1, int n2) {
+
+        return n1/n2;
+    }
+}
---
+++ src/es/avalon/objetos/ejemplo17/Principal.java
@@ -0,0 +1,10 @@
+package es.avalon.objetos.ejemplo17;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Matematicas m= new Matematicas();
+        System.out.println(m.sumar(2,2));
+    }
+}
