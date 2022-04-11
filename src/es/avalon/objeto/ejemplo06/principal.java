4ece84013374349cdac76bda20fe68cd1c6730dc
Mercedes <mercedes.alfonzo20@gmail.com>
2022-04-11 17:30:57 -0400

    SobreCarga de Metodos

---
+++ src/es/avalon/objeto/ejemplo06/principal.java
@@ -0,0 +1,14 @@
+package es.avalon.objeto.ejemplo06;
+
+public class principal {
+    public static void main(String[] args) {
+        rectangulo R= new rectangulo(2,3);
+        System.out.println(R.area());
+        System.out.println(R.perimetro());
+
+
+
+    }
+
+
+}
