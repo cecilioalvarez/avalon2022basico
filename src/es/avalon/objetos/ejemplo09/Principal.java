8c80a949b76a7afb5fd1a125086e8d6bd9212517
Nerys <beatrizalvaradocona@gmail.com>
2022-04-11 17:38:58 -0400

    Multiples Constructores

---
+++ src/es/avalon/objetos/ejemplo09/Principal.java
@@ -0,0 +1,17 @@
+package es.avalon.objetos.ejemplo09;
+
+public class Principal {
+    public static void main(String[] args){
+
+       // int num=7;
+       // num++;
+        //System.out.println(num);
+        //num++;
+       // System.out.println(num);
+
+        int numero=0;
+        for (int i=0;i<10;i++){
+            System.out.println(numero+i);
+        }
+    }
+}
