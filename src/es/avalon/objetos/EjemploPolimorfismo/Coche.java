e31eefe3ae0298c80d4b6fc30ec6fdca5583d5d9
dcamposmagaldi <dcamposmagaldi@gmail.com>
2022-04-11 16:34:54 +0200

    Ejemplo Abstracto

--- src/es/avalon/objetos/EjemploPolimorfismo/Coche.java
+++ src/es/avalon/objetos/EjemploPolimorfismo/Coche.java
@@ -1,14 +1,10 @@
 package es.avalon.objetos.EjemploPolimorfismo;
 
-public class Coche {
+public abstract class Coche {
 
-    public void girar() {
+    public abstract void girar();
 
-        System.out.println("el coche  gira");
-    }
 
-    public void cambiarMarcha() {
+    public abstract void cambiarMarcha();
 
-        System.out.println("el coche  cambia de marcha");
-    }
 }
