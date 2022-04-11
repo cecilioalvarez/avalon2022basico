3b58b074edd05b79e17a6df134e1bc72fd94dd3f
jcalat <jcalataruiz@hotmail.com>
2022-04-11 18:44:35 +0200

    equals y hashcode

--- src/es/avalon/objetos/Ejemplo20/Principal.java
+++ src/es/avalon/objetos/Ejemplo20/Principal.java
@@ -3,7 +3,15 @@ package es.avalon.objetos.Ejemplo20;
 public class Principal {
     public static void main(String[] args) {
 
-        Persona p=new Persona("72052532H","Pedro");
+        Persona p=new Persona("12345698Ñ","Pedro");
         System.out.println(p);
+        Persona p1=new Persona("12345698ñ","Pedro");
+
+        System.out.println(p.equals(p1));
+        System.out.println(p1.equals(p1));
+        System.out.println(p.hashCode()==p1.hashCode());
+        System.out.println(p.hashCode());
+        System.out.println(p1.hashCode());
+
     }
 }
