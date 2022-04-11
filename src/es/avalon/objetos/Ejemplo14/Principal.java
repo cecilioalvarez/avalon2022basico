c55f548b95bb74799a55a7f50785908b385e869b
Edwin <edwinstiv20@gmail.com>
2022-04-11 07:45:58 -0500

    exte

---
+++ src/es/avalon/objetos/Ejemplo14/Principal.java
@@ -0,0 +1,17 @@
+package es.avalon.objetos.Ejemplo14;
+
+public class Principal {
+    public static void main(String[] args) {
+        DiscoDuro d1= new DiscoDuro("samsung",2);
+        Ordenador o= new Ordenador(d1);
+        NAS minas= new NAS();
+        minas.addDisco(new DiscoDuro("seagate",2));
+        minas.addDisco(new DiscoDuro("seagate",2));
+        minas.addDisco(new DiscoDuro("seagate",2));
+        minas.addDisco(new DiscoDuro("seagate",2));
+        minas.addDisco(new DiscoDuro("seagate",2));
+        o.conectarNas(minas);
+        System.out.println(o.capacidadDisco());
+    }
+
+}
---
+++ src/es/avalon/objetos/ejemplo15/Deportista.java
@@ -0,0 +1,19 @@
+package es.avalon.objetos.ejemplo15;
+
+public class Deportista extends  Persona{
+    private String deporte;
+
+    public String getDeporte() {
+        return deporte;
+    }
+
+    public void setDeporte(String deporte) {
+        this.deporte = deporte;
+    }
+
+    public Deportista(String nombre, int edad, String deporte) {
+        super(nombre, edad);
+        this.deporte = deporte;
+
+    }
+}
