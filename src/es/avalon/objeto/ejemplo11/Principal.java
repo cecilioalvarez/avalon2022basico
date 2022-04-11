1f75c5bfbf989563fec3a901ca1898f2237b4097
Mercedes <mercedes.alfonzo20@gmail.com>
2022-04-11 17:42:42 -0400

    Relaciones Chaqueta

---
+++ src/es/avalon/objeto/ejemplo11/Principal.java
@@ -0,0 +1,31 @@
+package es.avalon.objeto.ejemplo11;
+import java.util.ArrayList;
+
+public class Principal {
+    public static void main(String[] args) {
+
+
+        ArrayList<Telefono> lista= new ArrayList<Telefono>();
+        lista.add(new Telefono("apple",123456789));
+        lista.add(new Telefono("oppo",123489789));
+        lista.add(new Telefono("samsung",44442223));
+        lista.add(new Telefono("xiaomi",544545433));
+
+        for (int i=0;i<lista.size();i++) {
+
+            System.out.println(lista.get(i).getMarca());
+            System.out.println(lista.get(i).getNumero());
+        }
+
+        lista.remove(2);
+        System.out.println("***************************");
+        for (int i=0;i<lista.size();i++) {
+
+            System.out.println(lista.get(i).getMarca());
+            System.out.println(lista.get(i).getNumero());
+        }
+
+
+    }
+
+}
