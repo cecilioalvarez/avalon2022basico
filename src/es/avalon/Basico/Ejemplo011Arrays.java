2df3659f03e0b95f5f327c135278faa93c2744da
Mercedes <mercedes.alfonzo20@gmail.com>
2022-04-11 16:14:11 -0400

    Clases y Objetos

---
+++ src/es/avalon/Basico/Ejemplo011Arrays.java
@@ -0,0 +1,16 @@
+package es.avalon.Basico;
+
+public class Ejemplo011Arrays {
+    public static void main(String[] arg) {
+        int [] lista= new int[3];
+        lista[0]=20;
+        lista[1]=30;
+        lista[2]=50;
+        System.out.println(lista[0]);
+        System.out.println(lista[1]);
+        System.out.println(lista[2]);
+
+        for(int i=0;i<lista.length;i++){
+    }
+    }
+}
