ff7f8e3960ca6fb9f8e2c6359e3364be86c775e3
Nerys <beatrizalvaradocona@gmail.com>
2022-04-11 17:30:50 -0400

    Bombilla Array Lampara

---
+++ src/es/avalon/objetos/ejemplo06/Principal.java
@@ -0,0 +1,13 @@
+package es.avalon.objetos.ejemplo06;
+
+
+public class Principal {
+    public static void main(String[] args) {
+        Rectangulo R= new Rectangulo(2,3);
+        System.out.println(R.area());
+        System.out.println(R.perimetro());
+
+
+
+    }
+}
