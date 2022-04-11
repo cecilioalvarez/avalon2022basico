d1b99a85b834bcd34e4f9cbdb207f74bec245b14
dcamposmagaldi <dcamposmagaldi@gmail.com>
2022-04-11 16:38:01 +0200

    Ejemplo Clase matemáticas

---
+++ src/es/avalon/objetos/Ejemplo017/Main.java
@@ -0,0 +1,8 @@
+package es.avalon.objetos.Ejemplo017;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Matematicas.sumar(2,2));
+    }
+}
---
+++ src/es/avalon/objetos/Ejemplo017/Matematicas.java
@@ -0,0 +1,24 @@
+package es.avalon.objetos.Ejemplo017;
+
+public class Matematicas {
+
+    public static double sumar (int n1, int n2) {
+
+        return n1+n2;
+    }
+
+    public static double restar (int n1, int n2) {
+
+        return n1-n2;
+    }
+
+    public static double producto (int n1, int n2) {
+
+        return n1*n2;
+    }
+
+    public static double division (int n1, int n2) {
+
+        return n1/n2;
+    }
+}
