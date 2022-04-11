429512f07064f3b9727f8c38d8c2fa715ec43815
jcalat <jcalataruiz@hotmail.com>
2022-04-11 18:10:16 +0200

    metodo toString

---
+++ src/es/avalon/objetos/Ejemplo20/Persona.java
@@ -0,0 +1,35 @@
+package es.avalon.objetos.Ejemplo20;
+
+public class Persona {
+    private String dni;
+    private String nombre;
+
+    public Persona(String dni, String nombre) {
+        this.dni = dni;
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
+}
---
+++ src/es/avalon/objetos/Ejemplo20/Principal.java
@@ -0,0 +1,9 @@
+package es.avalon.objetos.Ejemplo20;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Persona p=new Persona("72052532H","Pedro");
+        System.out.println(p);
+    }
+}
