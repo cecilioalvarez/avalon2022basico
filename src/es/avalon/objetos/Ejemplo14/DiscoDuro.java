46e5d0d500d6ebecea8df7520379aa5220d17cf6
Edwin <edwinstiv20@gmail.com>
2022-04-11 07:27:57 -0500

    DD

---
+++ src/es/avalon/objetos/Ejemplo14/DiscoDuro.java
@@ -0,0 +1,27 @@
+package es.avalon.objetos.Ejemplo14;
+
+public class DiscoDuro {
+    private String marca;
+    private double capacidad;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(double capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public DiscoDuro(String marca, double capacidad) {
+        this.marca = marca;
+        this.capacidad = capacidad;
+    }
+}
