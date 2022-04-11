266fc618fec111e5dad8e7cae794b4cf02470206
SirEikir <cristoferpuerto@gmail.com>
2022-04-11 18:13:24 +0200

    Ejemplo de toString();

---
+++ src/es/avalon/objetos/ejemplo20/Main.java
@@ -0,0 +1,8 @@
+package es.avalon.objetos.ejemplo20;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona p= new Persona ("12345678a", "Pedro");
+        System.out.println(p);
+    }
+}
---
+++ src/es/avalon/objetos/ejemplo20/Persona.java
@@ -0,0 +1,35 @@
+package es.avalon.objetos.ejemplo20;
+
+public class Persona {
+    private String dni;
+    private String nombre;
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
+
+    public Persona(String dni, String nombre) {
+        this.dni = dni;
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
