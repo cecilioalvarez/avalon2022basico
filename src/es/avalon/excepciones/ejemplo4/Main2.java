ccf1f2ad7d60eab1185dc7ea922e4a54ee96bdb5
SirEikir <cristoferpuerto@gmail.com>
2022-04-12 18:27:58 +0200

    Actualizacion de archivos y Uso de Fechas y Date and SimpleDateFormat

---
+++ src/es/avalon/excepciones/ejemplo4/Main2.java
@@ -0,0 +1,30 @@
+package es.avalon.excepciones.ejemplo4;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Main2 {
+    public static void main(String[] args) {
+
+        SimpleDateFormat formateador = new SimpleDateFormat("dd M yy");
+        Date fecha = null;
+        try {
+            fecha= formateador.parse("20 10 2025");
+        } catch (ParseException e) {
+           System.out.println("Ha habido un error con la fecha");
+        }
+
+
+        Calendar calendario = new GregorianCalendar();
+        calendario.setTime(fecha);
+
+        System.out.println(calendario.get(Calendar.DAY_OF_MONTH));
+        System.out.println(calendario.get(Calendar.MONTH));
+        System.out.println(calendario.get(Calendar.YEAR));
+
+        System.out.println(formateador.format(fecha));
+    }
+}
