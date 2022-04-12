fb37e8233fcaa4eb04fd493a7320afeb095e1825
Nerys <beatrizalvaradocona@gmail.com>
2022-04-12 13:15:33 -0400

    Formateador fechas 2

---
+++ src/es/avalon/excepciones/ejemplo4/Principal.java
@@ -0,0 +1,16 @@
+package es.avalon.excepciones.ejemplo4;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Principal {
+    public static void main(String[] args){
+        // que tiene asignado ahora
+        Date fecha=new Date();
+        Calendar calendario=new GregorianCalendar();
+        calendario.setTime(fecha);
+
+        System.out.println(calendario.get(Calendar.DAY_OF_MONTH));
+    }
+}
---
+++ src/es/avalon/excepciones/ejemplo4/Principal2.java
@@ -0,0 +1,29 @@
+package es.avalon.excepciones.ejemplo4;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Principal2 {
+    public static void main(String[] args){
+
+        SimpleDateFormat formateador=new SimpleDateFormat("dd M yy");
+        Date fecha=null;
+        try {
+            fecha=formateador.parse("20 10 2025");
+        } catch (ParseException e) {
+            System.out.println("Hay un error con la fecha");
+
+        }
+        Calendar calendario=new GregorianCalendar();
+        calendario.setTime(fecha);
+
+        System.out.println(calendario.get(Calendar.DAY_OF_MONTH));
+        System.out.println(calendario.get(Calendar.MONTH));
+        System.out.println(calendario.get(Calendar.YEAR));
+
+        System.out.println(formateador.format(fecha));
+    }
+}
