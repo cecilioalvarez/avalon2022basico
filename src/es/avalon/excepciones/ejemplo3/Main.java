e863353a365ed62b4c2be76ef8496a8e96868ddd
SirEikir <cristoferpuerto@gmail.com>
2022-04-12 17:47:27 +0200

    Actualizacion de archivos

--- src/es/avalon/excepciones/ejemplo3/Main.java
+++ src/es/avalon/excepciones/ejemplo3/Main.java
@@ -1,9 +1,10 @@
 package es.avalon.excepciones.ejemplo3;
 
-
-
 public class Main {
     public static void main(String[] args) {
-        GestorFechas gestor = new GestorFechas();
+
+        GestorFechas gestor= new GestorFechas();
+        System.out.println(gestor.sumaDias("10/01/2022","11/01/2022","12/01/2022"));
+
     }
 }
