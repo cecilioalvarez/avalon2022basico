dff4ca1e2733fc2285f5d08be328adbe16b1b1b9
Edwin <edwinstiv20@gmail.com>
2022-04-12 10:14:35 -0500

    fecha

---
+++ src/es/avalon/exepciones/ejemplo02/Principal.java
@@ -0,0 +1,27 @@
+package es.avalon.exepciones.ejemplo02;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Principal {
+    public static void main(String[] args) {
+        Date fecha=null;
+        SimpleDateFormat formateador= new SimpleDateFormat("dd/m/yy");
+        try {
+            fecha=formateador.parse("20/12/2022");
+            Calendar calendario=new GregorianCalendar();
+            calendario.setTime(fecha);
+            System.out.println(calendario.get(Calendar.DAY_OF_MONTH));
+            System.out.println(calendario.get(Calendar.WEEK_OF_MONTH));
+            System.out.println(calendario.get(Calendar.DAY_OF_YEAR));
+            System.out.println(calendario.get(Calendar.MONTH)+1);
+            System.out.println(calendario.get(Calendar.YEAR));
+        } catch (ParseException e) {
+            System.out.println("fecha incorrecta");
+            System.out.println(e.getMessage());
+        }
+    }
+}
