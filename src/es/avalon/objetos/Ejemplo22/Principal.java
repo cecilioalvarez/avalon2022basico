a9d2dd30ffbb926146fbdac29769409d65eefec8
jcalat <jcalataruiz@hotmail.com>
2022-04-12 15:00:11 +0200

    interfaz pelicula Cancion

--- src/es/avalon/objetos/Ejemplo22/Principal.java
+++ src/es/avalon/objetos/Ejemplo22/Principal.java
@@ -5,16 +5,16 @@ import java.util.ArrayList;
 public class Principal {
 
     public static void main(String[] args) {
-        ArrayList <Controles> elementos=new ArrayList<Controles>();
+        ArrayList <Controles> elementos= new ArrayList<>();
 
         elementos.add(new Cancion("El tiburon"));
         elementos.add(new Pelicula("Titanic"));
 
-        for (int i=0;i< elementos.size();i++){
-            elementos.get(i).play();
-            elementos.get(i).stop();
-            elementos.get(i).next();
-            elementos.get(i).back();
+        for (Controles elemento : elementos) {
+            elemento.play();
+            elemento.stop();
+            elemento.next();
+            elemento.back();
         }
     }
 }
