3bfc5518232e1598ea8bdd3904918fa3b8465278
Edwin <edwinstiv20@gmail.com>
2022-04-12 12:02:49 -0500

    fechass ejercicio

---
+++ src/es/avalon/exepciones/ejemplo05/Persona.java
@@ -0,0 +1,41 @@
+package es.avalon.exepciones.ejemplo05;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Persona {
+     private String nombre;
+    int edad;
+    private Date fechanacimiento;
+
+    public Date getFechanacimiento() {
+        return fechanacimiento;
+    }
+
+    public void setFechanacimiento(Date fechanacimiento) {
+        this.fechanacimiento = fechanacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+}
