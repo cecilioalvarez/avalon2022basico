48366f32cb56d4b5e172153c05a38a4db4ea3457
SirEikir <cristoferpuerto@gmail.com>
2022-04-12 14:06:53 +0200

    Hashcode e Equals

--- src/es/avalon/objetos/ejemplo20/Main.java
+++ src/es/avalon/objetos/ejemplo20/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Persona p= new Persona ("12345678a", "Pedro");
         System.out.println(p);
+
+        Persona p2 = new Persona ("3216259J", "Juan");
+        System.out.println(p.equals(p2));
     }
 }
--- src/es/avalon/objetos/ejemplo20/Persona.java
+++ src/es/avalon/objetos/ejemplo20/Persona.java
@@ -1,5 +1,7 @@
 package es.avalon.objetos.ejemplo20;
 
+import java.util.Objects;
+
 public class Persona {
     private String dni;
     private String nombre;
@@ -10,6 +12,19 @@ public class Persona {
                 "dni='" + dni + '\'' +
                 ", nombre='" + nombre + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Persona)) return false;
+        Persona persona = (Persona) o;
+        return dni.equals(persona.dni);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dni);
     }
 
     public Persona(String dni, String nombre) {
