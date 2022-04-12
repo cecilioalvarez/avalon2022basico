c3d0e70a2df8e7d36ebb4b8206b3250e9ba8fb25
dcamposmagaldi <dcamposmagaldi@gmail.com>
2022-04-12 17:59:02 +0200

    Fechas Multiples

--- src/es/avalon/Excepciones/EjercicioFecha/GestorFecha.java
+++ src/es/avalon/Excepciones/EjercicioFecha/GestorFecha.java
@@ -6,22 +6,34 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class GestorFecha {
 
 
     public int sumaDias(String fecha1, String fecha2, String fecha3) {
         DateFormat formateador = new SimpleDateFormat("d/M/yy");
-        int total=0;
+        int total = 0;
         try {
-            Date f1=formateador.parse(fecha1);
-            Date f2=formateador.parse(fecha2);
-            Date f3=formateador.parse(fecha3);
-            Calendar c1=
+            Date f1 = formateador.parse(fecha1);
+            Date f2 = formateador.parse(fecha2);
+            Date f3 = formateador.parse(fecha3);
+            Calendar c1 = new GregorianCalendar();
+            c1.setTime(f1);
+            Calendar c2 = new GregorianCalendar();
+            c2.setTime(f2);
+            Calendar c3 = new GregorianCalendar();
+            c3.setTime(f3);
+            int d1 = c1.get(Calendar.DAY_OF_MONTH);
+            int d2 = c2.get(Calendar.DAY_OF_MONTH);
+            int d3 = c3.get(Calendar.DAY_OF_MONTH);
+
+            total = d1 + d2 + d3;
 
 
         } catch (ParseException e) {
             e.printStackTrace();
         }
+        return total;
     }
 }
--- src/es/avalon/Excepciones/EjercicioFecha/Main.java
+++ src/es/avalon/Excepciones/EjercicioFecha/Main.java
@@ -1,4 +1,10 @@
 package es.avalon.Excepciones.EjercicioFecha;
 
 public class Main {
+    public static void main(String[] args) {
+
+        GestorFecha gestor= new GestorFecha();
+        System.out.println(gestor.sumaDias("10/10/2022","11/07/2020","12/07/2020"));
+
+    }
 }
