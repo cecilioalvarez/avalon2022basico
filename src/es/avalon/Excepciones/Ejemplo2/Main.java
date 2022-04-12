0aa307679330b7c2d94378ed4cb2f279e832981b
dcamposmagaldi <dcamposmagaldi@gmail.com>
2022-04-12 17:17:10 +0200

    Ejemplo Excepciones Fecha

---
+++ src/es/avalon/Excepciones/Ejemplo2/Main.java
@@ -0,0 +1,31 @@
+package es.avalon.Excepciones.Ejemplo2;
+
+import es.avalon.objetos.EjemploInterfaz2.Pelicula;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Main {
+    public static void main(String[] args) {
+        Date fecha=null;
+        DateFormat formateador = new SimpleDateFormat("dd/M/yy");
+
+
+        try{
+            fecha=formateador.parse("12/4/2022");
+            Calendar calendario= new GregorianCalendar();
+            calendario.setTime(fecha);
+            System.out.println(calendario.get(Calendar.YEAR));
+            System.out.println(calendario.get(Calendar.MONTH)+1);
+            System.out.println(calendario.get(Calendar.MONTH));
+            System.out.println(calendario.get(Calendar.DATE));
+        }catch (ParseException e){
+            System.out.println(e.getMessage());
+            System.out.println("Fecha incorrecta");
+        }
+    }
+}
