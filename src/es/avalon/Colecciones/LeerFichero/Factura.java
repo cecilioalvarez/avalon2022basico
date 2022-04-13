e2dbf99276eb3882f689c274a0efce73693f5205
dcamposmagaldi <dcamposmagaldi@gmail.com>
2022-04-13 17:46:05 +0200

    Sacar precio factura desde un fichero.txt

---
+++ src/es/avalon/Colecciones/LeerFichero/Factura.java
@@ -0,0 +1,34 @@
+package es.avalon.Colecciones.LeerFichero;
+
+public class Factura {
+
+    private int id;
+    private String producto;
+    private float precio;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+
+}
---
+++ src/es/avalon/Colecciones/LeerFichero/Main.java
@@ -0,0 +1,39 @@
+package es.avalon.Colecciones.LeerFichero;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Factura> listaFactura= new ArrayList<Factura>();
+
+        try {
+            Scanner scanner = new Scanner((new File("factura.txt")));
+            while(scanner.hasNextLine()){
+                String linea=scanner.nextLine();
+                String[] precio = linea.split(",");
+                Factura factura= new Factura();
+                factura.setPrecio(Integer.parseInt(precio[2]));
+                listaFactura.add(factura);
+
+
+
+            }
+            int total=0;
+            for (Factura factura:listaFactura){
+                total+=factura.getPrecio();
+            }
+            System.out.println(total);
+
+
+            scanner.close();
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+
+        }
+    }
+}
