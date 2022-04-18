073747fc0181bcc0fb64bdcb9d9c0a92b890005c
Edwin <edwinstiv20@gmail.com>
2022-04-18 10:37:32 -0500

    testearpersonaas

--- src/es/avalon/testing/PersonaTest.java
+++ src/es/avalon/testing/PersonaTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 
 import java.time.LocalDate;
 
-import static org.testng.AssertJUnit.assertTrue;
+import static org.testng.AssertJUnit.*;
 
 public class PersonaTest {
     @DisplayName("la persona es mayor de edad")
@@ -18,4 +18,23 @@ public class PersonaTest {
         assertTrue(p1.esMayorDeEdad());
 
     }
+    @Test
+    public void testGestorPersonaBusqueda(){
+        GestorPersona gestor= new GestorPersona();
+        Persona p= gestor.buscarPersonaPorDni("i");
+        assertNotNull(p);
+        Persona p2 =gestor.buscarPersonaPorDni("10");
+        assertNull(p2);
+
+    }
+    @Test
+    public  void testComparacionPersonas (){
+        Persona p1 = new Persona("1","edwin",LocalDate.of(2000,10,10));
+        Persona p2 = new Persona("2","pedro",LocalDate.of(2010,10,10));
+        Persona pNueva = new Persona("3","stiven",LocalDate.of(1980,10,10));
+
+       // assertEquals(p1,Persona.esMayor(p1,p2));
+    }
+
+
 }
