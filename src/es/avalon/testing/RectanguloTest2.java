017da928c823e9f71264aacad970ae42ad827c0a
SirEikir <cristoferpuerto@gmail.com>
2022-04-18 17:40:33 +0200

    Actualizacion de archivos

--- src/es/avalon/testing/RectanguloTest2.java
+++ src/es/avalon/testing/RectanguloTest2.java
@@ -1,36 +1,69 @@
 package es.avalon.testing;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@DisplayName("Test sobre el calculo de areas del cuadrado")
 public class RectanguloTest2 {
 
-    Rectangulo rectangulo;
+    Rectangulo rectangulo1;
     Rectangulo rectangulo2;
 
-    public void setup(){
+    @BeforeEach
+    public void setUp() {
 
+        rectangulo1= new Rectangulo(2,2);
+        rectangulo2= new Rectangulo(4,3);
+    }
+    @DisplayName("Test sobre el calculo de area del Rectangulo")
+
+    @Test
+    public void rectanguloCalculoAreaTest() {
+
+
+        assertEquals(4,rectangulo1.area());
+        assertEquals(12,rectangulo2.area());
+    }
+    @DisplayName("Test sobre el calculo de perimetro del rectangulo")
+
+    @Test
+    public void rectanguloCalculoPerimetroTest() {
+
+        assertEquals(8,rectangulo1.perimetro());
+        assertEquals(14,rectangulo2.perimetro());
+    }
+
+    @DisplayName("Test sobre el calculo de areas con valor 0")
+
+    @Test
+    public void rectanguloCalculoAreaLadosCeroTest() {
+
+        Rectangulo rectangulo1= new Rectangulo(0,0);
+        Rectangulo rectangulo2= new Rectangulo(0,2);
+        Rectangulo rectangulo3= new Rectangulo(2,0);
+        assertEquals(0,rectangulo1.area());
+        assertEquals(0,rectangulo2.area());
+        assertEquals(0,rectangulo3.area());
+    }
+    @DisplayName("Test sobre captura de Excepciones con lados negativos")
+
+    @Test
+    public void rectanguloCalculoAreaExcepcionTest() {
+
+        Rectangulo rectangulo1= new Rectangulo(-1,0);
+        // expresiones lambda
+        Exception exception = assertThrows(Exception.class, () -> {
+            rectangulo1.area();
+        });
     }
 
 
-        @Test
-        public void rectanguloCalculoAreaTest(){
-            Rectangulo r = new Rectangulo(2,2);
-            assertEquals(4, r.area());
-        }
-        @Test
-        public void rectanguloCalculoPerimetroTest(){
+    @AfterEach
+    public  void tearDown() {
 
-            Rectangulo r = new Rectangulo(2,2);
-            assertEquals( 8, r.perimetro());
-        }
-        @Test
-        public void rectanguloCalculoAreaExceptioTest(){
-            Rectangulo rectangulo1 = new Rectangulo(-1, 0);
-            Exception exception = assertThrows(Exception.class, () ->{
-                rectangulo1.area();
-            });
-        }
+        rectangulo1=null;
+        rectangulo2=null;
     }
+}
