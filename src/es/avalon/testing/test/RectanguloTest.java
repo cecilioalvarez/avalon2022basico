e51aecf5f466a6e2a6af9054611b28b8f4ce5adb
Edwin <edwinstiv20@gmail.com>
2022-04-18 12:19:58 -0500

    rectang

--- src/es/avalon/testing/test/RectanguloTest.java
+++ src/es/avalon/testing/test/RectanguloTest.java
@@ -1,37 +1,59 @@
 package es.avalon.testing.test;
 
 import es.avalon.testing.Rectangulo;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.testng.AssertJUnit.assertEquals;
 
 public class RectanguloTest {
-    @Test
+    @org.junit.jupiter.api.Test
     public void rectanguloCalculoAreaTest() {
 
         Rectangulo rectangulo1= new Rectangulo(2,2);
         Rectangulo rectangulo2= new Rectangulo(4,3);
-        assertEquals(4,rectangulo1.area());
-        assertEquals(12,rectangulo2.area());
+        Assertions.assertEquals(4,rectangulo1.area());
+        Assertions.assertEquals(12,rectangulo2.area());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void rectanguloCalculoAreaLadosCeroTest() {
 
         Rectangulo rectangulo1= new Rectangulo(0,0);
         Rectangulo rectangulo2= new Rectangulo(0,2);
         Rectangulo rectangulo3= new Rectangulo(2,0);
-        assertEquals(0,rectangulo1.area());
-        assertEquals(0,rectangulo2.area());
-        assertEquals(0,rectangulo3.area());
+        Assertions.assertEquals(0,rectangulo1.area());
+        Assertions.assertEquals(0,rectangulo2.area());
+        Assertions.assertEquals(0,rectangulo3.area());
     }
 
+    @org.junit.jupiter.api.Test
+    public void rectanguloCalculoAreaExcepcionTest() {
+
+        Rectangulo rectangulo1= new Rectangulo(-1,0);
+        // expresiones lambda
+        Exception exception = assertThrows(Exception.class, () -> {
+            rectangulo1.area();
+        });
+
+
+
+    }
     @Test
     public void rectanguloCalculoPerimetroTest() {
 
+        //arrange (inicializar)
         Rectangulo r= new Rectangulo(2,2);
         Rectangulo r2= new Rectangulo(3,2);
-        assertEquals(8,r.perimetro());
-        assertEquals(10,r2.perimetro());
+
+        // actuar
+        double area= r.perimetro();
+        double area2= r2.perimetro();
+
+
+        // confirmar comprobar
+        Assertions.assertEquals(8,area);
+        Assertions.assertEquals(10,area2);
     }
 }
