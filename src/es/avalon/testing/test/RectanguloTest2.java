c8f2d5951e74323d620c975577fd9c1b410621e2
Edwin <edwinstiv20@gmail.com>
2022-04-18 12:22:36 -0500

    rectangtest

--- src/es/avalon/testing/test/RectanguloTest2.java
+++ src/es/avalon/testing/test/RectanguloTest2.java
@@ -1,14 +1,14 @@
 package es.avalon.testing.test;
 
 import es.avalon.testing.Rectangulo;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.testng.AssertJUnit.assertEquals;
 
+@DisplayName("Test sobre la clase rectangulo")
 public class RectanguloTest2 {
+
     Rectangulo rectangulo1;
     Rectangulo rectangulo2;
 
@@ -19,32 +19,35 @@ public class RectanguloTest2 {
         rectangulo2= new Rectangulo(4,3);
     }
 
-    @Test
+    @DisplayName("Test calculo Area del Rectangulo")
+    @org.junit.jupiter.api.Test
     public void rectanguloCalculoAreaTest() {
 
 
-        assertEquals(4,rectangulo1.area());
-        assertEquals(12,rectangulo2.area());
+        Assertions.assertEquals(4,rectangulo1.area());
+        Assertions.assertEquals(12,rectangulo2.area());
     }
-    @Test
+    @org.junit.jupiter.api.Test
+    @DisplayName("Test calculo Perimetro del Rectangulo")
     public void rectanguloCalculoPerimetroTest() {
 
-        assertEquals(8,rectangulo1.perimetro());
-        assertEquals(14,rectangulo2.perimetro());
+        Assertions.assertEquals(8,rectangulo1.perimetro());
+        Assertions.assertEquals(14,rectangulo2.perimetro());
     }
 
-
-    @Test
+    @DisplayName("Test Area para valores de lado 0")
+    @org.junit.jupiter.api.Test
     public void rectanguloCalculoAreaLadosCeroTest() {
 
         Rectangulo rectangulo1= new Rectangulo(0,0);
         Rectangulo rectangulo2= new Rectangulo(0,2);
         Rectangulo rectangulo3= new Rectangulo(2,0);
-        assertEquals(0,rectangulo1.area());
-        assertEquals(0,rectangulo2.area());
-        assertEquals(0,rectangulo3.area());
+        Assertions.assertEquals(0,rectangulo1.area());
+        Assertions.assertEquals(0,rectangulo2.area());
+        Assertions.assertEquals(0,rectangulo3.area());
     }
 
+    @DisplayName("Test Lanzamientor de Excepcion calculo Area lado negativo")
     @Test
     public void rectanguloCalculoAreaExcepcionTest() {
 
@@ -61,5 +64,4 @@ public class RectanguloTest2 {
 
         rectangulo1=null;
         rectangulo2=null;
-    }
-}
+    }}
