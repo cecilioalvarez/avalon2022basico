1cfda7a3d5dc49c61ebd0b23df636cd146cec589
dcamposmagaldi <dcamposmagaldi@gmail.com>
2022-04-18 16:09:40 +0200

    Testing tdd

--- src/es/avalon/Testing/RectanguloTest2.java
+++ src/es/avalon/Testing/RectanguloTest2.java
@@ -1,13 +1,11 @@
 package es.avalon.Testing;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@DisplayName("Test sobre la clase rectangulo")
 public class RectanguloTest2 {
 
         //@Test
@@ -18,26 +16,27 @@ public class RectanguloTest2 {
         Rectangulo rectangulo1;
         Rectangulo rectangulo2;
 
+        //Inicializando valores del Rectangulo"
         @BeforeEach
         public void setUp(){
             rectangulo1=new Rectangulo(2,2);
             rectangulo2=new Rectangulo(4,3);
         }
-
+        @DisplayName("Test Calculo Area del Rectangulo")
         @Test
         public void rectanguloCalculoAreaTest() {
 
             assertEquals(4,rectangulo1.area());
             assertEquals(12,rectangulo2.area());
         }
-
+        @DisplayName("Test Calculo Perimetro del Rectangulo")
         @Test
         public void rectanguloCalculoPerimetroTest() {
 
             assertEquals(8,rectangulo1.perimetro());
             assertEquals(14,rectangulo2.perimetro());
         }
-
+        //Reseteo valores del Rectangulo
         @AfterEach
         public void tearDown(){
             rectangulo1=null;
