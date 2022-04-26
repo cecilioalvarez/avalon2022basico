9a2089a8c173547c9774667bd1df096435322311
Edwin <edwinstiv20@gmail.com>
2022-04-26 08:33:10 -0500

    motores

--- src/es/avalon/testing/test/CocheTest.java
+++ src/es/avalon/testing/test/CocheTest.java
@@ -5,12 +5,16 @@ import es.avalon.testing.Motor;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CocheTest {
 
     @Test
     public  void CochePotenciaTest(){
-        Coche c= new Coche("ausi",new Motor(150));
+        Motor m= mock(Motor.class);
+        when(m.getPotencia()).thenReturn(109.5);
+        Coche c= new Coche("ausi",m);
         assertEquals(100,c.getPotencia(),1);
     }
 }
