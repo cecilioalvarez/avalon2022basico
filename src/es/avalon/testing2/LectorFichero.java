fd9040f39d6ff9debcd423d6da9ae6940b19819a
Edwin <edwinstiv20@gmail.com>
2022-04-26 09:34:53 -0500

    motoresss

--- src/es/avalon/testing2/LectorFichero.java
+++ src/es/avalon/testing2/LectorFichero.java
@@ -1,4 +1,30 @@
 package es.avalon.testing2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class LectorFichero {
+    private File fichero;
+
+    public LectorFichero(File fichero) {
+        this.fichero = fichero;
+    }
+
+    public List<String> leerLineas() {
+        List<String> lineas = new ArrayList<>();
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(fichero);
+            while (scanner.hasNextLine()) {
+                lineas.add(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return lineas;
+    }
 }
