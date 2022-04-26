7c303e28fc8a07002ac8bdd6fad0c2da2ae58fef
cecilioAlvarez <cmac@arquitecturajava.com>
2022-04-26 18:14:04 +0200

    Creando el gestor de Empresas

--- src/es/avalon/testing2/Empresa.java
+++ src/es/avalon/testing2/Empresa.java
@@ -1,13 +1,18 @@
 package es.avalon.testing2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Empresa {
     private String nombre;
-
+    private List<Gasto> gastos =new ArrayList<>();
 
     public String getNombre() {
         return nombre;
     }
-
+    public void addGasto(Gasto gasto) {
+        gastos.add(gasto);
+    }
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
---
+++ src/es/avalon/testing2/Gasto.java
@@ -0,0 +1,18 @@
+package es.avalon.testing2;
+
+public class Gasto {
+
+    private double importe;
+
+    public double getImporte() {
+        return importe;
+    }
+
+    public void setImporte(double importe) {
+        this.importe = importe;
+    }
+
+    public Gasto(double importe) {
+        this.importe = importe;
+    }
+}
