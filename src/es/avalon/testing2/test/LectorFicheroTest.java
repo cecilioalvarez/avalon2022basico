f247a8713e66344ae8fcf0796bcc4f3d7ec0cbea
cecilioAlvarez <cmac@arquitecturajava.com>
2022-04-26 17:55:09 +0200

    lectorficherostest

--- src/es/avalon/testing2/test/LectorFicheroTest.java
+++ src/es/avalon/testing2/test/LectorFicheroTest.java
@@ -16,39 +16,27 @@ public class LectorFicheroTest {
 
     @Test
     @DisplayName("leer lineas de fichero")
-    public void leerLineasTest() {
+    public void leerLineasTest() throws FileNotFoundException {
         //arrange preprar el codigo
-        LectorFichero lector= new LectorFichero(new File("gastos.txt"));
+        LectorFichero lector = new LectorFichero(new File("gastos.txt"));
         //actuar ACT
-        try {
-            List<String> lineas=lector.leerLineas();
-            //verificar Assert
-            assertTrue(lineas.size()>=1);
-            assertTrue(lineas.get(0).contains("empresa"));
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        List<String> lineas = lector.leerLineas();
+        //verificar Assert
+        assertTrue(lineas.size() >= 1);
+        assertTrue(lineas.get(0).contains("empresa"));
 
 
     }
 
     @Test
     @DisplayName("leer lineas de fichero vacio")
-    public void leerLineasFicheroLineasVaciasTest() {
+    public void leerLineasFicheroLineasVaciasTest() throws FileNotFoundException {
         //arrange preprar el codigo
-        LectorFichero lector= new LectorFichero(new File("gastosvacio.txt"));
+        LectorFichero lector = new LectorFichero(new File("gastosvacio.txt"));
         //actuar ACT
-        try {
-            List<String> lineas=lector.leerLineas();
-            //verificar Assert
-            assertTrue(lineas.size()==0);
-
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
+        List<String> lineas = lector.leerLineas();
+        //verificar Assert
+        assertTrue(lineas.size() == 0);
 
     }
 
@@ -56,18 +44,11 @@ public class LectorFicheroTest {
     @DisplayName("excepcion al no existir el fichero")
     public void noExisteFicheroTest() {
         //arrange preprar el codigo
-        LectorFichero lector= new LectorFichero(new File("noexiste.txt"));
-
+        LectorFichero lector = new LectorFichero(new File("noexiste.txt"));
         Assertions.assertThrows(FileNotFoundException.class, () -> {
-          lector.leerLineas();
+            lector.leerLineas();
         });
-
-
     }
-
-
-
-
 
 
 }
