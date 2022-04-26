84bd028d7db4f3be47ba48148ec4c1035b4b1d2b
alejandro <yesid.alejo300@gmail.com>
2022-04-26 11:00:10 -0500

    test 5

---
+++ src/Test/empresasFicheros/Empresa.java
@@ -0,0 +1,17 @@
+package Test.empresasFicheros;
+
+public class Empresa {
+    private String nombre;
+
+    public Empresa(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
