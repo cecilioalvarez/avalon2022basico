ceb04bdfcb7caf3fec1de47fbdf8bfdb566b8aa7
SirEikir <cristoferpuerto@gmail.com>
2022-04-27 14:43:31 +0200

    miscelaneous

--- src/es/avalon/testing2/test/GestorGastosEmpresaTest.java
+++ src/es/avalon/testing2/test/GestorGastosEmpresaTest.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class GestorGastoEmpresaTest {
+class GestorGastoEmpresaTest {
 
     @Test
     @DisplayName("leer los datos de las empresas y sus gastos del fichero de texto")
