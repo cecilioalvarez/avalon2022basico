a4e28d3c2d0df8a96de19973c6b04da3cdf4c36b
SirEikir <cristoferpuerto@gmail.com>
2022-04-27 14:36:11 +0200

    miscelaneous

--- src/es/avalon/testing2/test/GestorGastosEmpresaTest.java
+++ src/es/avalon/testing2/test/GestorGastosEmpresaTest.java
@@ -1,10 +1,43 @@
 package es.avalon.testing2.test;
 
+import es.avalon.testing2.Empresa;
+import es.avalon.testing2.GestorGastoEmpresa;
+import es.avalon.testing2.LectorFichero;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class GestorGastosEmpresaTest {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class GestorGastoEmpresaTest {
 
     @Test
-    @DisplayName("")
+    @DisplayName("leer los datos de las empresas y sus gastos del fichero de texto")
+    public void leerEmpresaGastosDeFichero() throws FileNotFoundException {
+
+
+        //geera dependencias entre los diferentes Test
+        LectorFichero lector=mock(LectorFichero.class);
+        List<String> lineas= new ArrayList<>();
+        lineas.add("empresaa, 100,200,400,600");
+        lineas.add("empresab, 50,100,200,25");
+
+        when(lector.leerLineas()).thenReturn(lineas);
+
+        GestorGastoEmpresa gestor = new GestorGastoEmpresa(lector);
+        List<Empresa> listaEmpresas = gestor.getEmpresasConGastos();
+
+        assertTrue(listaEmpresas.contains(new Empresa("empresaa")));
+        assertTrue(listaEmpresas.contains(new Empresa("empresab")));
+
+
+    }
 }
+
