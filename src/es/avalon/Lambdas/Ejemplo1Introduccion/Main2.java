040535f60c94a53ab0d994efa28ae6921f9b1a36
SirEikir <cristoferpuerto@gmail.com>
2022-04-28 16:04:18 +0200

    Clases de Comparator y CompareTo Parte 3

--- src/es/avalon/Lambdas/Ejemplo1Introduccion/Main2.java
+++ src/es/avalon/Lambdas/Ejemplo1Introduccion/Main2.java
@@ -1,9 +1,6 @@
 package es.avalon.Lambdas.Ejemplo1Introduccion;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Main2 {
     public static void main(String[] args) {
@@ -13,6 +10,11 @@ public class Main2 {
         Persona p4 = new Persona("Maria",60);
         // es la clase conjunto que permite tener una coleccion de elementos sin que haya repetidos
         // Set es un interface, TreeSet es una implementacion del interface
+
+        // Clase anonima
+        Comparator<Persona> comparadorNombre = (Persona persona, Persona t1) -> {
+                return persona.getNombre().compareTo(t1.getNombre());
+        };
         Set<Persona> conjunto = new TreeSet<>(new ComparadorPersonaNombre());
         conjunto.add(p1);
         conjunto.add(p2);
