25f790ef706d799e4030312d379866bd6a03cd13
SirEikir <cristoferpuerto@gmail.com>
2022-04-28 15:49:35 +0200

    Clases de Comparator y CompareTo Parte 2

--- src/es/avalon/Lambdas/Ejemplo1Introduccion/ComparadorPersonaEdad.java
+++ src/es/avalon/Lambdas/Ejemplo1Introduccion/ComparadorPersonaEdad.java
@@ -1,9 +1,19 @@
 package es.avalon.Lambdas.Ejemplo1Introduccion;
 
-public class ComparadorPersonaEdad implements Comparable<Persona>{
+import java.util.Comparator;
 
+public class ComparadorPersonaEdad  implements Comparator<Persona> {
     @Override
-    public int compareTo(Persona persona) {
-        return 0;
+    public int compare(Persona o1, Persona o2) {
+        //nos devuelve uno si la persona es mayor 0 si es igual -1 si es menor
+        if (o1.getEdad()>o2.getEdad()) {
+
+            return 1;
+        }else if (o1.getEdad()< o2.getEdad()) {
+            return -1;
+        }else {
+            return 0;
+        }
     }
+
 }
