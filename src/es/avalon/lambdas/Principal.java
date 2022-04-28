6a798784faa14682769626ac265e5a9299fbfd9e
cecilioAlvarez <cmac@arquitecturajava.com>
2022-04-28 15:05:08 +0200

    Comparable y Comparator

---
+++ src/es/avalon/lambdas/Principal.java
@@ -0,0 +1,33 @@
+package es.avalon.lambdas;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Persona p1= new Persona("pedro",20);
+        Persona p2= new Persona ("ana",30);
+        System.out.println(p1.compareTo(p2));
+        System.out.println(p2.compareTo(p1));
+        System.out.println(p2.compareTo(p2));
+        ArrayList<Persona> lista= new ArrayList<Persona>();
+
+        lista.add(p1);
+        lista.add(p2);
+        lista.add(new Persona("miguel",50));
+        lista.add(new Persona ("maria",60));
+        //se apoya en compareto para tener la lista ordenada
+        Collections.sort(lista);
+
+        for (Persona p: lista) {
+
+            System.out.println(p.getNombre());
+            System.out.println(p.getEdad());
+        }
+
+
+
+    }
+}
---
+++ src/es/avalon/lambdas/Principal2.java
@@ -0,0 +1,31 @@
+package es.avalon.lambdas;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Principal2 {
+    public static void main(String[] args) {
+        Persona p1 = new Persona("pedro", 20);
+        Persona p2 = new Persona("ana", 30);
+        Persona p3 = new Persona("miguel", 50);
+        Persona p4 = new Persona("maria", 60);
+
+        // es la clase conjunto que permite tener una coleccion de elementos sin que haya repetidos
+        //Set es un interface ,TreeSet es una implementación del interface
+        Set<Persona> conjunto= new TreeSet<>(new ComparadorPersonaNombre());
+        conjunto.add(p4);
+
+        conjunto.add(p1);
+        conjunto.add(p2);
+        conjunto.add(p3);
+
+
+        for (Persona p: conjunto) {
+
+            System.out.println(p.getNombre());
+            System.out.println(p.getEdad());
+        }
+    }
+}
