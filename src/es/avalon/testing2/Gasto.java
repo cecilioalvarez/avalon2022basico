d1f7d3fcd3ddcea0964e3647ea5af3020791a506
Mercedes <mercedes.alfonzo20@gmail.com>
2022-05-04 17:31:12 -0400

    Pruebas Unitarias con gestor y lineas ligados

---
+++ src/es/avalon/testing2/Gasto.java
@@ -0,0 +1,33 @@
+package es.avalon.testing2;
+
+import java.util.Objects;
+
+public class Gasto {
+
+    private double importe;
+
+    public double getImporte() {
+        return importe;
+    }
+
+    public void setImporte(double importe) {
+        this.importe = importe;
+    }
+
+    public Gasto(double importe) {
+        this.importe = importe;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Gasto)) return false;
+        Gasto gasto = (Gasto) o;
+        return Double.compare(gasto.importe, importe) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(importe);
+    }
+}
---
+++ src/es/avalon/testing2/LectorFichero.java
@@ -0,0 +1,35 @@
+package es.avalon.testing2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LectorFichero {
+
+    private File fichero;
+
+    public LectorFichero(File fichero) {
+        this.fichero = fichero;
+    }
+
+    public List<String> leerLineas() throws FileNotFoundException {
+        List<String> lineas = new ArrayList<>();
+
+        try ( Scanner scanner = new Scanner(fichero)){
+            while (scanner.hasNextLine()) {
+                String linea=scanner.nextLine();
+                if(linea.contains("empresa")) {
+                    lineas.add(linea);
+                }
+
+            }
+        } catch (FileNotFoundException e) {
+            throw e;
+        }
+
+        return lineas;
+    }
+
+}
